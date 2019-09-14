e7ab91cfbc9ea19e30492361650c9e5420455dd9
alex-ovo nathan <avo.nathan@gmail.com>
2019-09-14 21:27:13 +0100

    created division functionality

--- kivyapp.py
+++ kivyapp.py
@@ -30,23 +30,33 @@ class Controller(GridLayout):
 
 
     def divide_button(self, instance):
-        # self.na = '\u20A6'
-        inflow = int(self.inflow.text)
+        inflow = float(self.inflow.text)
         self.inflow.text = ""
         if not inflow:
             self.add_widget(Label(text="Please, put an appropriate amount!"))
         else:
+            self.per_10 = inflow * 0.1
+            self.per_15 = inflow * 0.15
+            self.per_20 = inflow * 0.2
+            self.per_25 = inflow * 0.25
             
-            self.tithe = str(inflow * 0.1)
-            self.family = str(inflow * 0.1)
-            self.preserve = str(inflow * 0.15)
-            self.seeds = str(inflow * 0.2)
-            self.misc = str(inflow * 0.2)
-            self.major = str(inflow * 0.25)
+            self.tithe = str(self.per_10)
+            self.family = str(self.per_10)
+            self.preserve = str(self.per_15)
+            self.seeds = str(self.per_20)
+            self.misc = str(self.per_20)
+            self.major = str(self.per_25)
+            self.total = self.per_10 + self.per_10 + self.per_15 + self.per_20 + self.per_20 + self.per_25
             # self.add_widget(Label())   
 
         with open(f"Division details for {self.n}{inflow}.docx", "w", encoding='utf-8') as d:
-            d.write(f"Tithe - 10%: {self.n}{self.tithe}")  
+            d.write(f"""Tithe(10%): {self.n}{self.tithe}
+Family(10%): {self.n}{self.family}
+Preserve(15%): {self.n}{self.preserve}
+Seeds/Offerrings(20%): {self.n}{self.seeds}
+Miscellaneous(20%): {self.n}{self.misc}
+Major Project(25%): {self.n}{self.major}
+Total(100%): {self.n}{self.total}""")  
             
         # self.add_widget(Label(text=f"Division for {self.n}{inflow}"))     
         
